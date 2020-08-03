bf261a6f6d5070126d00fae7903413de1e407778
gaBerwithHD <glp.xd24@gmail.com>
2020-08-04 01:55:07 +0200

    Project setup

---
+++ src/main/java/me/madfix/thelab/bungeecordchatlogger/ProjectMain.java
@@ -0,0 +1,16 @@
+package me.madfix.thelab.bungeecordchatlogger;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ProjectMain extends Plugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
